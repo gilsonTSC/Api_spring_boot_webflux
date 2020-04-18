9ae9a8cfe76ee469b44e8db50010d2bf07aa8962
Gilson <gilson.costa@dce.ufpb.br>
2020-04-18 11:35:57 -0300

    Criando um serviço reativo funcional

---
+++ src/main/java/com/gilson/webflux/PlaylistHandler.java
@@ -0,0 +1,43 @@
+package com.gilson.webflux;
+
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.gilson.webflux.document.Playlist;
+import com.gilson.webflux.services.PlaylistService;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class PlaylistHandler {
+
+	@Autowired
+	PlaylistService service;
+	
+	public Mono<ServerResponse> findAll(ServerRequest request){
+		return ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(this.service.findAll(), Playlist.class);
+	}
+	
+	public Mono<ServerResponse> findById(ServerRequest request){
+		String id = request.pathVariable("id");
+		return ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(this.service.findById(id), Playlist.class);
+	}
+	
+	public Mono<ServerResponse> save(ServerRequest request){
+		final Mono<Playlist> playlist = request.bodyToMono(Playlist.class);
+		return ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(fromPublisher(playlist.flatMap(this.service::save), Playlist.class));
+	}
+	
+}
